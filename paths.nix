64026ed84100940ecbfbe4056b2f072138155d00
David Arnold <david.arnold@iohk.io>
2023-02-24 20:43:39 -0500

    feat: improve error reporting
    - make the previous commit less painful for downstream
    - overhaul all error while at it

--- paths.nix
+++ paths.nix
@@ -1,15 +1,15 @@
 rec {
   cellPath = cellsFrom: cellName: {
     __toString = _: "${cellsFrom}/${cellName}";
-    rel = ''\${cellsFrom}/${cellName}'';
     readme = "${cellsFrom}/${cellName}/Readme.md";
+    rel = "${builtins.baseNameOf cellsFrom}/${cellName}";
   };
   cellBlockPath = cellPath': cellBlock: {
     __toString = _: "${cellPath'}/${cellBlock.name}";
     file = "${cellPath'}/${cellBlock.name}.nix";
     file' = "${cellPath'.rel}/${cellBlock.name}.nix";
     dir = "${cellPath'}/${cellBlock.name}/default.nix";
-    dir' = "${cellPath'.rel}/${cellBlock.name}.nix";
+    dir' = "${cellPath'.rel}/${cellBlock.name}/default.nix";
     readme = "${cellPath'}/${cellBlock.name}/Readme.md";
   };
   targetPath = cellsFrom: cellName: cellBlock: let
