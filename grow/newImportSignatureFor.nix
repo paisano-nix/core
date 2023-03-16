d27d672bf4003c573541d865fd18cf86135fe87d
guangtao <gtrunsec@hardenedlinux.org>
2023-03-15 17:01:20 -0700

    fix: the sourceInfo to merge into nixpkgs

--- grow/newImportSignatureFor.nix
+++ grow/newImportSignatureFor.nix
@@ -7,15 +7,17 @@ This file implements the unique import signature of each block.
 }: cfg: let
   self = cfg.inputs.self.sourceInfo // {rev = cfg.inputs.self.sourceInfo.rev or "not-a-commit";};
   instantiateNixpkgsWith = system:
-    if cfg.nixpkgsConfig != {}
-    then
-      (import cfg.inputs.nixpkgs {
-        inherit system;
-        config = cfg.nixpkgsConfig;
-      })
-      // {inherit (cfg.inputs.nixpkgs) sourceInfo;}
-    # numtide/nixpkgs-unfree blocks re-import
-    else cfg.inputs.nixpkgs.legacyPackages.${system};
+    (
+      if cfg.nixpkgsConfig != {}
+      then
+        (import cfg.inputs.nixpkgs {
+          inherit system;
+          config = cfg.nixpkgsConfig;
+        })
+      # numtide/nixpkgs-unfree blocks re-import
+      else cfg.inputs.nixpkgs.legacyPackages.${system}
+    )
+    // {inherit (cfg.inputs.nixpkgs) sourceInfo;};
 in
   system: cells: (
     (deSystemize system cfg.inputs)
