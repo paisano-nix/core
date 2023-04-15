5475a1ed67ea33ec5cca18a10322aac212d9bf76
David Arnold <david.arnold@iohk.io>
2023-04-15 17:48:25 -0500

    refactor: simplify local env

--- local/flake.nix
+++ local/flake.nix
@@ -1,51 +1,47 @@
 {
   description = "Paisano Core development shell";
+  inputs.nosys.url = "github:divnix/nosys";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.main.url = "path:../.";
-  outputs = inputs: let
-    inherit (inputs.nixpkgs) lib;
-    eachSystem = f:
-      lib.genAttrs
-      lib.systems.flakeExposed
-      (system:
-        f (
-          inputs.nixpkgs.legacyPackages.${system}.appendOverlays [inputs.devshell.overlays.default]
-          // {namaka = inputs.main.inputs.namaka.packages.${system}.default;}
-        ));
-    inherit (lib.stringsWithDeps) noDepEntry;
-  in {
-    devShells = eachSystem (pkgs: let
-      checkMod = {
-        commands = [{package = pkgs.treefmt;}];
-        packages = [
-          pkgs.alejandra
-          pkgs.shfmt
-          pkgs.nodePackages.prettier
-          pkgs.nodePackages.prettier-plugin-toml
-        ];
-        devshell.startup.nodejs-setuphook = noDepEntry ''
-          export NODE_PATH=${
-            pkgs.nodePackages.prettier-plugin-toml
-          }/lib/node_modules:$NODE_PATH
-        '';
-      };
-    in {
-      check = pkgs.devshell.mkShell {
-        name = "Paisano Core (Check)";
-        imports = [checkMod];
-      };
-      default = pkgs.devshell.mkShell {
-        name = "Paisano Core";
-        imports = [checkMod];
-        commands = [
-          {package = pkgs.namaka;}
-          {
-            package = pkgs.cocogitto;
-            name = "cog";
-          }
-        ];
-      };
-    });
-  };
+  outputs = inputs @ {nosys, ...}:
+    nosys (inputs.main.inputs // inputs) (
+      {
+        self,
+        namaka,
+        nixpkgs,
+        devshell,
+        ...
+      }:
+        with nixpkgs.legacyPackages;
+        with nixpkgs.legacyPackages.nodePackages;
+        with devshell.legacyPackages; let
+          inherit (lib.stringsWithDeps) noDepEntry;
+          checkMod = {
+            commands = [{package = treefmt;}];
+            packages = [alejandra shfmt prettier prettier-plugin-toml];
+            devshell.startup.nodejs-setuphook = noDepEntry ''
+              export NODE_PATH=${prettier-plugin-toml}/lib/node_modules:$NODE_PATH
+            '';
+          };
+        in {
+          devShells = {
+            check = mkShell {
+              name = "Paisano Core (Check)";
+              imports = [checkMod];
+            };
+            default = mkShell {
+              name = "Paisano Core";
+              imports = [checkMod];
+              commands = [
+                {package = namaka.packages.default;}
+                {
+                  package = cocogitto;
+                  name = "cog";
+                }
+              ];
+            };
+          };
+        }
+    );
 }
